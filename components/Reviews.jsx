019224239eb99f54a121de3cfd7e810e432fc9a0
LeKhoaDev <lephamanhkhoa201203@gmail.com>
2024-07-30 22:11:07 +0700

    portfolio commit update

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -13,38 +13,38 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 const reviewsData = [
     {
-        avatar: '././reviews/avatar-1.png',
+        avatar: '/reviews/avatar-1.png',
         name: 'Richard Thompson',
         job: 'Chef',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
 
     },
     {
-        avatar: '././reviews/avatar-2.png',
+        avatar: '/reviews/avatar-2.png',
         name: 'Evelyn Anderson',
         job: 'Interior Designer',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
     },
     {
-        avatar: '././reviews/avatar-3.png',
+        avatar: '/reviews/avatar-3.png',
         name: 'John Doe',
         job: 'Game Dev',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
     },
     {
-        avatar: '././reviews/avatar-4.png',
+        avatar: '/reviews/avatar-4.png',
         name: 'Emily Smith',
         job: 'Therapist',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
     },
     {
-        avatar: '././reviews/avatar-5.png',
+        avatar: '/reviews/avatar-5.png',
         name: 'Oliver Taylor',
         job: 'Engineer',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
     },
     {
-        avatar: '././reviews/avatar-6.png',
+        avatar: '/reviews/avatar-6.png',
         name: 'Mason Wilson',
         job: 'Editor',
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id augue sit amet felis blandit pulvinar.',
--- components/Work.jsx
+++ components/Work.jsx
@@ -15,7 +15,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 const projectData = [
     {
-        image: '././work/h5.png',
+        image: '/work/h5.png',
         category: 'react js',
         name: 'Traveling Website',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
@@ -23,7 +23,7 @@ const projectData = [
         github: 'https://github.com/LeKhoaDev/ReactJs-Traveling/tree/main/travelproject'
     },
     {
-        image: '././work/h7.png',
+        image: '/work/h7.png',
         category: 'react js',
         name: 'Cinema Website',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
@@ -31,7 +31,7 @@ const projectData = [
         github: 'https://github.com/LeKhoaDev/project-cinema'
     },
     {
-        image: '././work/h4.png',
+        image: '/work/h4.png',
         category: 'react js',
         name: 'Clothes Website',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
@@ -39,7 +39,7 @@ const projectData = [
         github: 'https://github.com/LeKhoaDev/React-ClotheShop/tree/main/frontend'
     },
     {
-        image: '././work/h1.png',
+        image: '/work/h1.png',
         category: 'android',
         name: 'Food Booking App',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
@@ -47,7 +47,7 @@ const projectData = [
         github: 'https://github.com/LeKhoaDev/Android-FoodApp'
     },
     {
-        image: '././work/h2.png',
+        image: '/work/h2.png',
         category: 'android',
         name: 'Films App',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
@@ -55,7 +55,7 @@ const projectData = [
         github: 'https://github.com/LeKhoaDev/Android-Film'
     },
     {
-        image: '././work/h6.png',
+        image: '/work/h6.png',
         category: 'fullstack',
         name: 'Doctor Appointment Booking Website',
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod imperdiet metus in aliquet.',
