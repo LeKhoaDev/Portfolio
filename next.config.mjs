f7f6926ad62af366cd6eb5da1b539bcb5e9a5971
LeKhoaDev <lephamanhkhoa201203@gmail.com>
2024-07-30 22:39:45 +0700

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: "export",
-    basePath: "/Portfolio/",
-    assetPrefix: "/Portfolio/"
+    output: "export"
 };
 
 export default nextConfig;
